adf33e93f46cdb5307387910de5cf53190f7c88e
pswierad-src <piotrswierad.mail@gmail.com>
2025-12-02 23:04:34 +0100

    Day 2 ⭐⭐ 🎁🏬

---
+++ AdventOfCode2025/Day2/PartOne.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2025.Day2;
+
+public class PartOne : IPartOne
+{
+    public void Execute(string filePath)
+    {
+        var lines = File.ReadAllText(filePath);
+
+        var inputRanges = lines.Split(',')
+            .Select(r => r.Split('-'))
+            .Select(r => (long.Parse(r[0]), long.Parse(r[1])))
+            .ToList();
+        
+        var invalidIds = new List<long>();
+
+        foreach (var range in inputRanges)
+        {
+            for (var id = range.Item1; id <= range.Item2; id++)
+            {
+                var number = id.ToString();
+                var length = number.Length;
+                
+                if(length % 2 != 0)
+                    continue;
+                
+                var firstHalf = number[..(length / 2)];
+                var secondHalf = number.Substring(length / 2, length / 2);
+                
+                if(firstHalf == secondHalf)
+                    invalidIds.Add(id);
+            }
+            
+        }
+        
+        Console.WriteLine($"Sum of invalid IDs: {invalidIds.Sum()}");
+    }
+}
---
+++ AdventOfCode2025/Day2/PartTwo.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode2025.Day2;
+
+public class PartTwo : IPartTwo
+{
+    public void Execute(string filePath)
+    {
+        var lines = File.ReadAllText(filePath);
+
+        var inputRanges = lines.Split(',')
+            .Select(r => r.Split('-'))
+            .Select(r => (long.Parse(r[0]), long.Parse(r[1])))
+            .ToList();
+        
+        var invalidIds = new List<long>();
+
+        foreach (var range in inputRanges)
+        {
+            for (var id = range.Item1; id <= range.Item2; id++)
+            {
+                var number = id.ToString();
+                var length = number.Length;
+                
+                for (var patternLength = 1; patternLength <= length / 2; patternLength++)
+                {
+                    if (length % patternLength != 0) 
+                        continue;
+                    
+                    var pattern = number[..patternLength];
+
+                    if (!number.Where((t, i) => t != pattern[i % pattern.Length]).Any())
+                    {
+                        invalidIds.Add(id);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        Console.WriteLine($"Sum of invalid IDs: {invalidIds.Sum()}");
+    }
+}
