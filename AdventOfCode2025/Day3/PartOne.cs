6858c6d2d83f12abb31efc5be5b086b3806c945a
pswierad-src <piotrswierad.mail@gmail.com>
2025-12-03 21:07:02 +0100

    Day 3 ⭐⭐ 🔋⚡

---
+++ AdventOfCode2025/Day3/PartOne.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2025.Day3;
+
+public class PartOne : IPartOne
+{
+    public void Execute(string filePath)
+    {
+        var batteryBanks = File.ReadAllLines(filePath);
+
+        var joltages = new List<int>();
+
+        foreach (var bank in batteryBanks)
+        {
+            var batteries = bank.ToCharArray();
+
+            var highestJoltage = 0;
+
+            for (var i = 0; i < batteries.Length; i++)
+            {
+                for (var j = i+1; j < batteries.Length; j++)
+                {
+                    var currentJoltage = int.Parse($"{batteries[i]}{batteries[j]}");
+                    
+                    if(currentJoltage > highestJoltage)
+                        highestJoltage = currentJoltage;
+                }
+            }
+            
+            joltages.Add(highestJoltage);
+        }
+
+        Console.WriteLine($"Sum of highest joltages: {joltages.Sum()}");
+    }
+}
---
+++ AdventOfCode2025/Day3/PartTwo.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2025.Day3;
+
+public class PartTwo : IPartTwo
+{
+    public void Execute(string filePath)
+    {
+        var batteryBanks = File.ReadAllLines(filePath);
+
+        var joltages = new List<long>();
+
+        foreach (var bank in batteryBanks)
+        {
+            var batteries = bank.Select(c => int.Parse(c.ToString())).ToArray();
+            var maxBatteries = new int[12];
+            long bankJoltage = 0;
+            for (var i = maxBatteries.Length - 1; i >= 0; i--)
+            {
+                var startAt = i == maxBatteries.Length - 1 ? 0 : maxBatteries[i + 1] + 1;
+                maxBatteries[i] = startAt;
+                for (var cellToCheck = startAt; cellToCheck < batteries.Length - i; cellToCheck++)
+                {
+                    if (batteries[cellToCheck] > batteries[maxBatteries[i]])
+                        maxBatteries[i] = cellToCheck;
+                }
+                
+                bankJoltage += batteries[maxBatteries[i]] * (long)Math.Pow(10, i);
+            }
+            
+            joltages.Add(bankJoltage);
+        }
+
+        Console.WriteLine($"Sum of highest joltages: {joltages.Sum()}");
+    }
+}
