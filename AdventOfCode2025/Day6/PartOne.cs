9a70170b915332fa39dcdd6dcfbd062e8a89f2de
pswierad-src <piotrswierad.mail@gmail.com>
2025-12-07 18:14:28 +0100

    Day 6 ⭐⭐ 📝🔄

---
+++ AdventOfCode2025/Day6/PartOne.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace AdventOfCode2025.Day6;
+
+public class PartOne : IPartOne
+{
+    public void Execute(string filePath)
+    {
+        var input = File.ReadAllLines(filePath);
+
+        var numbers = new List<List<long>>();
+
+        var operations = input[^1].Split(' ')
+            .Select(s => s.Trim())
+            .Where(s => !string.IsNullOrWhiteSpace(s))
+            .ToList();
+
+        for (var i = 0; i < input.Length - 1; i++)
+        {
+            var n = input[i].Split(' ')
+                .Select(s => s.Trim())
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(long.Parse)
+                .ToList();
+            
+            numbers.Add(n);
+        }
+
+        var totals = new List<long>();
+
+        for (var i = 0; i < operations.Count; i++)
+        {
+            var elements = numbers.Select(n => n[i]).ToList();
+
+            var subTotal = operations[i] switch
+            {
+                "*" => elements.Aggregate(1L, (acc, x) => acc * x),
+                "+" => elements.Sum(),
+            };
+            
+            totals.Add(subTotal);
+        }
+
+        Console.WriteLine($"Grand total is {totals.Sum()}");
+    }
+}
---
+++ AdventOfCode2025/Day6/PartTwo.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2025.Day6;
+
+public class PartTwo : IPartTwo
+{
+    public void Execute(string filePath)
+    {
+        var input = File.ReadAllLines(filePath);
+        var digits = input.Select(line => line.ToList()).ToList();
+        var totals = new List<long>();
+
+        while (digits[^1].Count > 0)
+        {
+            var endOfColumn = false;
+            var operation = ' ';
+            
+            var rotatedNumbers = new List<string>();
+            
+            while (!endOfColumn)
+            {
+                var currentNumber = string.Empty;
+                
+                foreach (var line in digits)
+                {
+                    var lastOper = line[^1];
+                    line.RemoveAt(line.Count - 1);
+
+                    if (lastOper is '+' or '*')
+                    {
+                        endOfColumn = true;
+                        operation = lastOper;
+                        break;
+                    }
+
+                    currentNumber += lastOper;
+                }
+
+                rotatedNumbers.Add(currentNumber);
+            }
+
+            rotatedNumbers = [.. rotatedNumbers.Where(n => !string.IsNullOrWhiteSpace(n))];
+
+            var subTotal = operation switch
+            {
+                '*' => rotatedNumbers.Select(n => n.Trim().TrimEnd()).Select(long.Parse).Aggregate(1L, (acc, x) => acc * x),
+                '+' => rotatedNumbers.Select(n => n.Trim().TrimEnd()).Select(long.Parse).Sum()
+            };
+
+            totals.Add(subTotal);
+        }
+
+        Console.WriteLine($"Grand total is {totals.Sum()}");
+    }
+}
